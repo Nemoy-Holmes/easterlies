5e9751ca55482723ed244261cdd35db39abf0b5a
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-03-26 12:16:09 +0200

    adding date

---
+++ db/migrate/20180321114855_add_date.rb
@@ -0,0 +1,4 @@
+class AddDate < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321051851) do
+ActiveRecord::Schema.define(version: 20180326101240) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180321051851) do
     t.string "image_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
   end
 
 end
