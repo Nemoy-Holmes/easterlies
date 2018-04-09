476325651f6d42481286604c5ebdcbebe26c7b70
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-04-09 22:44:02 +0200

    4.7

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  get 'simple_pages/index'
-  root 'simple_pages#index'
+  root 'products#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326101240) do
+ActiveRecord::Schema.define(version: 20180406094131) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180326101240) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "date"
+    t.string "description"
   end
 
 end
