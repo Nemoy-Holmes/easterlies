d26540e4e9310599c4e24af4cc330a67eaa0f8a2
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-07-19 01:38:04 +0200

    5.11 heroku migration3

--- db/migrate/20180718213225_drop_products.rb
+++ db/migrate/20180718213225_drop_products.rb
@@ -1,16 +1,4 @@
 class DropProducts < ActiveRecord::Migration[5.1]
-  def up
-    drop_table :comments do |t|
-      t.integer "user_id"
-      t.text "body"
-      t.integer "product_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.integer "post_id"
-      t.index ["post_id"]
-      t.index ["product_id"]
-      t.index ["user_id"],
-    end
-
+  def change
   end
 end
