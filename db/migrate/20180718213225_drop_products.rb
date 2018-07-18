51e84762522a23e9ba0e2f46ef090b90e0dd60b2
Nemoy-Holmes <amitsingh20oct10@gmail.com>
2018-07-19 01:26:33 +0200

    5.11 heroku migration1

--- db/migrate/20180718213225_drop_products.rb
+++ db/migrate/20180718213225_drop_products.rb
@@ -1,6 +1,6 @@
 class DropProducts < ActiveRecord::Migration[5.1]
   def change
-    change_table : comments do |t|
+    change_table :comments do |t|
       remove_foreign_key :comments, name: "index_comments_on_post_id"
       remove_foreign_key :comments, name: "index_comments_on_product_id"
       remove_foreign_key :comments, name: "index_comments_on_user_id"
